5dfdfbe752b3c1e71a4ddd41b8490a58c9047d1c
joaopaulocosta551 <kenocosta44@gmail.com>
2023-02-20 23:27:01 -0300

    implementation of the pie chart

--- index.js
+++ index.js
@@ -15,3 +15,28 @@
     }
   });
 
+
+//   Segundo Gráfico
+
+  var cty = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(cty, {
+    type: 'pie',
+    data: {
+      labels: ['Fatia 1', 'Fatia 2', ],
+      datasets: [{
+        data: [10, 20, ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          
+        ]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+
+
+
